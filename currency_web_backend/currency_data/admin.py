943d923149dcb0994d482748bb9e63e70052c210
wlunareve <wlunareve@gmail.com>
2018-12-19 22:17:57 +0800

    Plus db to admin

--- currency_web_backend/currency_data/admin.py
+++ currency_web_backend/currency_data/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import JPY, CNY, USD, EUR, GBP, AUD, SGD
 # Register your models here.
+
+admin.site.register(JPY)
+admin.site.register(CNY)
+admin.site.register(USD)
+admin.site.register(EUR)
+admin.site.register(GBP)
+admin.site.register(AUD)
+admin.site.register(SGD)
